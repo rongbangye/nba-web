0a0d1e7780c8f4c6445c20289352bf09573ea319
rong8 <brianyoy@gmail.com>
2020-03-29 23:29:04 -0700

    updated SearchBar - finished stage3

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,35 +2,41 @@ import React, { Component } from 'react';
 
 import nba from '../nba-client';
 import Profile from './Profile';
-import ShotChart from './ShotChart';
 import DataViewContainer from './DataViewContainer';
 import SearchBar from './SearchBar';
 
+import { DEFAULT_PLAYER_INFO } from '../constants';
+
 class Main extends Component {
     state = {
-        playerId: 201939,
-        playerInfo: {}
+        playerInfo: DEFAULT_PLAYER_INFO
     }
 
     componentDidMount() {
         window.nba = nba;
-        nba.stats.playerInfo({ PlayerID: nba.findPlayer('LeBron James').playerId })
-            .then((info) => {
-                // console.log(info);
-                const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
-                console.log(playInfo);
-                this.setState({ playerInfo: playInfo });
-            })
+        this.loadPlayerInfo('LeBron James');
     }
-
+    loadPlayerInfo = (playerName) => {
+        nba.stats.playerInfo({ PlayerID: nba.findPlayer(playerName).playerId}).then((info) => {
+            console.log(info);
+            const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+            console.log(playInfo);
+            this.setState({ playerInfo: playInfo });
+        })
+    }
+ 
+    handleSelectPlayer = (playerName) => {
+        this.loadPlayerInfo(playerName);
+    }
 
     render() {
         return (
             <div className="main">
-                <SearchBar />
+                <SearchBar 
+                handleSelectPlayer={this.handleSelectPlayer}/>
                 <div className="player">
                     <Profile playerInfo={this.state.playerInfo} />
-                    <DataViewContainer playerId={this.state.playerId} />
+                    <DataViewContainer playerId={this.state.playerInfo.playerId} />
                 </div>
             </div>
         );
